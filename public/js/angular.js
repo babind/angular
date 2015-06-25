11575a977317c64234a5e1b0cb6c64604ce25866
Babin Dahal <babin.dahal3@gmail.com>
2015-06-25 06:32:54 +0545

    extracted to js file

---
+++ public/js/angular.js
@@ -0,0 +1,22 @@
+	
+	angular.module('myApp', [])
+.run(function($rootScope) {
+// use .run to access $rootScope
+$rootScope.rootProperty = 'root scope';
+})
+.controller('ParentController', function($scope) {
+// use .controller to access properties inside `ng-controller`
+// in the DOM omit $scope, it is inferred based on the current controller
+$scope.parentProperty = 'parent scope';
+})
+.controller('ChildController', function($scope) {
+$scope.childProperty = 'child scope';
+// just like in the DOM, we can access any of the properties in the
+// prototype chain directly from the current $scope
+$scope.fullSentenceFromChild
+= 'Same $scope: We can access: ' +
+$scope.rootProperty + ' and ' +
+$scope.parentProperty + ' and ' +
+$scope.childProperty
+});
+
--- resources/views/posts/index.php
+++ resources/views/posts/index.php
@@ -3,55 +3,32 @@
 <html ng-app="myApp">
 <head>
 <title>Simple app</title>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.js">
-</script>
-<script>
-	angular.module('myApp', [])
-.run(function($rootScope) {
-// use .run to access $rootScope
-$rootScope.rootProperty = 'root scope';
-})
-.controller('ParentController', function($scope) {
-// use .controller to access properties inside `ng-controller`
-// in the DOM omit $scope, it is inferred based on the current controller
-$scope.parentProperty = 'parent scope';
-})
-.controller('ChildController', function($scope) {
-$scope.childProperty = 'child scope';
-// just like in the DOM, we can access any of the properties in the
-// prototype chain directly from the current $scope
-$scope.fullSentenceFromChild
-= 'Same $scope: We can access: ' +
-$scope.rootProperty + ' and ' +
-$scope.parentProperty + ' and ' +
-$scope.childProperty
-});
-</script>
-
-
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.js"></script>
+<script src="js/angular.js"></script>
 </head>
 
 <body>
-<my-directive>
-a</my-directive>
 <h1 ng-init="greeting = 'Hello World'">
 The greeting is: {{ greeting }}
 </h1>
-
 <p>We can access: {{ rootProperty }}</p>
-<div ng-controller="ParentController">
-<p>We can access: {{ rootProperty }}
-and {{ parentProperty }}</p>
-<div ng-controller="ChildController">
-<p>
-We can access:
-{{ rootProperty }} and
-{{ parentProperty }} and
-{{ childProperty }}
-</p>
-<p>{{ fullSentenceFromChild }}</p>
-</div>
-</div>
+	<div ng-controller="ParentController">
+			<p>We can access: {{ rootProperty }}
+			and {{ parentProperty }}</p>
+		<div ng-controller="ChildController">
+			<p>
+			We can access:
+			{{ rootProperty }} and
+			{{ parentProperty }} and
+			{{ childProperty }}
+			</p>
+			<p>{{ fullSentenceFromChild }}</p>
+		</div>
+	</div>
+	<div my-directive
+	my-url="http://google.com"
+	my-link-text="Click me to go to Google">
+	</div>
 
 </body>
 </html>
