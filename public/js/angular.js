7d3a6ad4107a6442ddb32f6f3238af9673eefd8c
Babin Dahal <babin.dahal3@gmail.com>
2015-07-07 22:17:55 +0545

    some changes in repo

--- public/js/angular.js
+++ public/js/angular.js
@@ -1,30 +1,11 @@
-angular.module('myApp', [])
-.directive('myDirective', function() {
+app.controller('MainCtrl', function($scope) {
+$scope.getCssClasses = function(ngModelContoller) {
 return {
-restrict: 'A',
-replace: true,
-scope: {
-myUrl: '@',
-// binding strategy
-myLinkText: '@' // binding strategy
-},
-template: '<a href="{{myUrl}}">' +
-'{{myLinkText}}</a>'
-}
-})
-
-angular.module('myApp', [])
-.run(function($rootScope, $timeout) {
-$timeout(function() {
-$rootScope.imgSrc = 'https://www.google.com/images/srpr/logo11w.png';
-}, 2000);
-});
-
-angular.module('myApp', [])
-.controller('EquationController', function($scope) {
-$scope.equation = {};
-$scope.change = function() {
-$scope.equation.output
-= Number($scope.equation.x) + 5;
+error: ngModelContoller.$invalid && ngModelContoller.$dirty,
+success: ngModelContoller.$valid && ngModelContoller.$dirty
+};
+};
+$scope.showError = function(ngModelController, error) {
+return ngModelController.$error[error];
 };
 });
--- resources/views/posts/index.php
+++ resources/views/posts/index.php
@@ -1,54 +1,29 @@
-<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<script type="text/javascript" src="js/app.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/app.css">
+	</head>
 
-<html ng-app="myApp">
-<head>
-<title>Simple app</title>
-<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.js"></script>
-<script src="js/angular.js"></script>
-</head>
+	<body>
+		<h1>Incredibly Basic Slider</h1>
+	<div id="slider">
+	  <a href="#" class="control_next">>></a>
+	  <a href="#" class="control_prev"><</a>
+	  <ul>
+	    <li>SLIDE 1</li>
+	    <li style="background: #aaa;">SLIDE 2</li>
+	    <li>SLIDE 3</li>
+	    <li style="background: #aaa;">SLIDE 4</li>
+	  </ul>  
+	</div>
 
-<body>
-<div my-directive
-my-url="http://google.com"
-my-link-text="Click me to go to Google"></div>
+	<div class="slider_option">
+	  <input type="checkbox" id="checkbox">
+	  <label for="checkbox">Autoplay Slider</label>
+	</div> 
 
-<h1>Wrong Way</h1>
-<img src="{{imgSrc}}" />
-<h1>Right way</h1>
-<img ng-src="{{imgSrc}}" />
 
-<div ng-controller="EquationController">
-<input type="text"
-ng-model="equation.x"
-ng-change="change()" />
-<code>{{ equation.output }}</code>
-</div>
-
-<form name="signup_form"
-ng-controller="FormController"
-ng-submit="submitForm()" novalidate>
-<div ng-repeat="field in fields"
-ng-form="signup_form_input">
-<input type="text"
-name="dynamic_input"
-ng-required="field.isRequired"
-ng-model="field.name"
-placeholder="{{field.placeholder}}" />
-<div
-ng-show="signup_form_input.dynamic_input.$dirty &&
-signup_form_input.dynamic_input.$invalid">
-<span class="error"
-ng-show="signup_form_input.dynamic_input.$error.required">
-The field is required.Built-In Directives
-97
-</span>
-</div>
-</div>
-<button type="submit"
-ng-disabled="signup_form.$invalid"
-	Submit all
-</button>
-</form>
-
+	</body>
 </body>
 </html>
